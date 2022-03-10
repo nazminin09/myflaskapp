b86d105a4eaf944e4bd33d931724f04aadd87094
unknown <nazminordin1999@gmail.com>
2022-03-10 10:05:53 +0800

    host change lagi

--- app.py
+++ app.py
@@ -277,6 +277,4 @@ def delete_article(id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.secret_key='secret123'
-    app.run(debug=True)
     app.run(host="0.0.0.0", port="8080")
